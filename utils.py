6384ca35d49b75106dbd641ef2eb4a974f46c088
Junghoon Seo <sjh14@gist.ac.kr>
2020-03-20 16:33:50 +0900

    Discretized Weight on lower optimization

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.autograd import Variable
 
 
@@ -21,20 +22,37 @@ class ExampleLabelWeights(nn.Module):
         self.params = self.params.cuda()
 
     def forward(self, losses, inputs_idx):
-        self.lastly_used_params = []
+        if self.training:
+            self.lastly_used_params = []
 
-        self.weights = []
-        for example_label_idx in inputs_idx:
-            example_label_weight = self.params[example_label_idx]
-            self.lastly_used_params.append(self.params[example_label_idx])
-            example_label_weight = torch.softmax(example_label_weight, dim=0)
-            self.weights.append(example_label_weight)
-        self.weights = torch.cat(self.weights, dim=0)
-        print(self.weights)
+            weights = []
+            for example_label_idx in inputs_idx:
+                example_label_weight = self.params[example_label_idx]
+                self.lastly_used_params.append(self.params[example_label_idx])
+                example_label_weight = torch.softmax(example_label_weight, dim=0)
+                weights.append(example_label_weight)
+            self.weights = torch.cat(weights, dim=0)
 
-        losses = losses * self.weights
-        self.lastly_used_params = torch.cat(self.lastly_used_params, dim=0)
-        return torch.sum(losses)
+            losses = losses * self.weights
+            self.lastly_used_params = torch.cat(self.lastly_used_params, dim=0)
+            return torch.sum(losses)
+        else:
+            weights = []
+            for example_label_idx in inputs_idx:
+                example_label_weight = self.params[example_label_idx]
+                example_label_weight = torch.softmax(example_label_weight, dim=0)
+                argmax_label = torch.argmax(example_label_weight, dim=0)
+                example_label_weight_onehot = torch.zeros_like(example_label_weight)
+                example_label_weight_onehot[argmax_label] = 1.
+
+                weights.append(example_label_weight_onehot)
+
+                # Initialize parameter
+                self.params[example_label_idx].data = torch.ones_like(self.params[example_label_idx].data)
+            weights = torch.cat(weights, dim=0)
+
+            losses = losses * weights
+            return torch.sum(losses)
 
     def update_last_used_weights(self, grads, lr):
         for param, grad in zip(self.params, grads):
