482230774bbe6740efe484a38b0347639ae4e5e7
Junghoon Seo <sjh14@gist.ac.kr>
2020-03-20 23:31:25 +0900

    [MSRCv2] Test Acc: 0.5485714

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 import copy
 
 import torch
-import numpy as np
 from tqdm import tqdm
 import torch.nn.functional as F
 
@@ -9,30 +8,34 @@ from torch.utils.data import DataLoader
 
 from datasets.datasets import Datasets
 from models.models import DeepModel
-from utils import to_torch_var, ExampleLabelWeights
+from utils import to_torch_var, ExampleLabelWeights, HLoss
 
 NUM_ITERATIONS = 5000
 NUM_ITERATIONS_ON_WEIGHTS = 20
-LEARNING_RATE = 1e-4
+LEARNING_RATE = 1e-3
 
 
 def main():
-    datasets = Datasets("Lost", test_fold=10, val_fold=10)
+    datasets = Datasets("MSRCv2", test_fold=10, val_fold=0)
 
     train_datasets = copy.deepcopy(datasets)
     train_datasets.set_mode('train')
-    train_dataloader = DataLoader(train_datasets, batch_size=32, num_workers=4, drop_last=False, shuffle=True)
+    train_dataloader = DataLoader(train_datasets, batch_size=64, num_workers=4, drop_last=True, shuffle=True)
 
+    h_loss = HLoss()
+
+    """
     val_datasets = copy.deepcopy(datasets)
     val_datasets.set_mode('val')
-    val_dataloader = DataLoader(val_datasets, batch_size=48, num_workers=4, drop_last=False, shuffle=True)
+    val_dataloader = DataLoader(val_datasets, batch_size=16, num_workers=4, drop_last=True, shuffle=True)
+    """
 
     test_datasets = copy.deepcopy(datasets)
     test_datasets.set_mode('test')
     test_dataloader = DataLoader(test_datasets, batch_size=16, num_workers=4, drop_last=False)
 
     train_cardinality = train_datasets.get_cardinality_possible_partial_set()
-    val_cardinality = val_datasets.get_cardinality_possible_partial_set()
+    # val_cardinality = val_datasets.get_cardinality_possible_partial_set()
 
     # Used in lower-loop
     example_label_weights = ExampleLabelWeights(train_cardinality)
@@ -43,11 +46,11 @@ def main():
     opt = torch.optim.Adam(model.params(), lr=LEARNING_RATE)
 
     train_data_iterator = iter(train_dataloader)
-    val_data_iterator = iter(val_dataloader)
+    # val_data_iterator = iter(val_dataloader)
     t = tqdm(range(NUM_ITERATIONS))
+
     for i in t:
         model.train()
-
         # Line 2) Get Batch from Training Dataset
         # Expected Question: "Why is this part so ugly?"
         # Answer: "Please refer https://github.com/pytorch/pytorch/issues/1917 ."
@@ -57,70 +60,31 @@ def main():
             train_data_iterator = iter(train_dataloader)
             data_train, y_partial_train, _, idx_train = next(train_data_iterator)
 
-        data_train = to_torch_var(data_train, requires_grad=False)
-        candidate_idx = torch.DoubleTensor(y_partial_train).cuda().nonzero()
-        data_train_with_candidates = data_train[candidate_idx[:, 0]].float()
-        y_train_with_candidates = to_torch_var(candidate_idx[:, 1], requires_grad=False)
-
-        # For lower-loop, set meta-model
-        # It's instantiation of separation of upper-loop parameter and lower-loop parameter.
-        meta_model = DeepModel(in_dim, out_dim).cuda()
-        meta_model.load_state_dict(model.state_dict())
-        meta_model.cuda()
-        meta_model.train()
-
-        example_label_weights.train()
-        # Line 4
-        y_f_hat = meta_model(data_train_with_candidates)
-        C = F.cross_entropy(y_f_hat, y_train_with_candidates, reduce=False)
-        # Line 5
-        # l_f_meta = torch.sum(C)
-        l_f_meta = example_label_weights(C, idx_train)
-
-        # Line 6: Get gradient thorough meta-model
-        meta_model.zero_grad()
-        grads = torch.autograd.grad(l_f_meta, (meta_model.params()), create_graph=True)
-        # Line 7: Update meta-model parameter
-        meta_model.update_params(LEARNING_RATE, source_params=grads)
-
-        # Ready for validation data batch
-        try:
-            data_val, y_partial_val, _, idx_val = next(val_data_iterator)
-        except StopIteration:
-            val_data_iterator = iter(val_dataloader)
-            data_val, y_partial_val, _, idx_val = next(val_data_iterator)
-
-        data_val = to_torch_var(data_val, requires_grad=False).float()
-        candidate_idx = torch.DoubleTensor(y_partial_val).cuda().nonzero(as_tuple=True)
-        # Line 8
-        meta_model.eval()
-        y_g_hat = meta_model(data_val)
-        y_g_hat_softmax = F.softmax(y_g_hat, dim=1)
-        y_g_hat_softmax_indexed = y_g_hat_softmax[candidate_idx]
-        y_g_hat_softmax_reduced = torch.split(y_g_hat_softmax_indexed,
-                                              val_cardinality[idx_val.numpy()].tolist())
-        y_g_hat_softmax_reduced_sum = []
-        for y_g_hat_softmax in y_g_hat_softmax_reduced:
-            y_g_hat_softmax_reduced_sum.append(torch.sum(y_g_hat_softmax))
-        y_g_hat_softmax_reduced_sum = torch.stack(y_g_hat_softmax_reduced_sum, dim=0)
-
-        t.set_description("Upper Loss: %s" % torch.mean(y_g_hat_softmax_reduced_sum).item())
-        l_g_meta = F.binary_cross_entropy(torch.clamp(y_g_hat_softmax_reduced_sum, 0., 1.),
-                                          torch.ones_like(y_g_hat_softmax_reduced_sum),
-                                          reduction='sum')
-
-        grad_eps = torch.autograd.grad(l_g_meta, example_label_weights.params, only_inputs=True, allow_unused=True)
-        example_label_weights.update_last_used_weights(grad_eps, 5e-1)
+        data_train = to_torch_var(data_train, requires_grad=False).float()
+        candidate_train_idx = torch.DoubleTensor(y_partial_train).cuda().nonzero(as_tuple=True)
 
         example_label_weights.eval()
         # Line 12
-        y_f_hat = model(data_train_with_candidates)
-        C = F.cross_entropy(y_f_hat, y_train_with_candidates, reduce=False)
-        l_f = example_label_weights(C, idx_train)
+        y_f_hat = model(data_train)
+        l_e = h_loss(y_f_hat)
+        y_f_hat_softmax_weighted = F.softmax(y_f_hat, dim=1)
+        y_f_hat_softmax_indexed = y_f_hat_softmax_weighted[candidate_train_idx]
+        y_f_hat_softmax_reduced = torch.split(y_f_hat_softmax_indexed,
+                                              train_cardinality[idx_train.numpy()].tolist())
+
+        y_f_hat_softmax_reduced_weighted_sum = []
+        for j, y_f_hat_softmax_weighted in enumerate(y_f_hat_softmax_reduced):
+            y_f_hat_softmax_reduced_weighted_sum.append(torch.sum(y_f_hat_softmax_weighted))
+        y_f_hat_softmax_reduced_weighted_sum = torch.stack(y_f_hat_softmax_reduced_weighted_sum, dim=0)
+        l_f = F.binary_cross_entropy(torch.clamp(y_f_hat_softmax_reduced_weighted_sum, 0., 1.),
+                                     torch.ones_like(y_f_hat_softmax_reduced_weighted_sum),
+                                     reduction='sum')
+
+        loss = l_f + 1e-3 * (i / NUM_ITERATIONS) * l_e
 
         # Line 13-14
         opt.zero_grad()
-        l_f.backward()
+        loss.backward()
         opt.step()
 
     """
