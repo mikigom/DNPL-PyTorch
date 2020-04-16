c48bd2b6fc85a30a8370728b4c1d29e615ee739f
Junghoon Seo <sjh14@gist.ac.kr>
2020-04-17 01:26:16 +0900

    Add yahoo exp script

--- exps_yahoo.py
+++ exps_yahoo.py
@@ -2,18 +2,20 @@ import numpy as np
 import math
 from train import main
 
-repeat = 20
-lambda_list = 10 ** np.arange(-4, 1.25, 0.25)
+repeat = 50
+epoch_list = (25, 50)
+lambda_list = 10 ** np.arange(-5, 3.5, 0.5)
 lambda_list = lambda_list.tolist()
 
 if __name__ == '__main__':
-    for lamd in lambda_list:
-        accs = list()
-        for i in range(repeat):
-            acc = main("Yahoo! News", lamd=lamd, num_epoch=25, use_norm=False)
-            accs.append(acc)
+    for epoch in epoch_list:
+        for lamd in lambda_list:
+            accs = list()
+            for i in range(repeat):
+                acc = main("Yahoo! News", lamd=lamd, num_epoch=epoch, use_norm=False)
+                accs.append(acc)
 
-        with open('exp_yahoo/lambda_%s.txt' % str(lamd), 'w') as f:
-            for acc in accs:
-                f.write("%s\n" % acc)
+            with open('exp_yahoo/epoch_%s_lambda_%s.txt' % str(epoch, lamd), 'w') as f:
+                for acc in accs:
+                    f.write("%s\n" % acc)
 
